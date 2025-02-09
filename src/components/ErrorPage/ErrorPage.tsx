a0174f59f3c716aedb944616b52abd75574c1d42
lh-rubz <habuelrub@gmail.com>
2025-02-09 23:20:49 +0200

    added an error page

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -1,16 +1,15 @@
-// ErrorPage.js
 import React, { useEffect } from "react";
 import { gsap } from "gsap";
-import "./ErrorPage.css"; // Make sure to create and import the CSS file
+import "./ErrorPage.css"; // Ensure the updated CSS file is imported
 
-const ErrorPage = ({ errorMessage = 'Uh Oh! Page not found!' }) => {  // Destructure the errorMessage prop here
+const ErrorPage = ({ errorMessage = 'Uh Oh! Page not found!' }) => {  
   useEffect(() => {
     // GSAP Animations
     let t1 = gsap.timeline();
     let t2 = gsap.timeline();
     let t3 = gsap.timeline();
 
-    t1.to(".cog1", {
+    t1.to(".cog1-1", {
       transformOrigin: "50% 50%",
       rotation: "+=360",
       repeat: -1,
@@ -18,7 +17,7 @@ const ErrorPage = ({ errorMessage = 'Uh Oh! Page not found!' }) => {  // Destruc
       duration: 8,
     });
 
-    t2.to(".cog2", {
+    t2.to(".cog2-1", {
       transformOrigin: "50% 50%",
       rotation: "-=360",
       repeat: -1,
@@ -27,43 +26,43 @@ const ErrorPage = ({ errorMessage = 'Uh Oh! Page not found!' }) => {  // Destruc
     });
 
     t3.fromTo(
-      ".wrong-para",
+      ".wrong-para-1",
       { opacity: 0 },
       { opacity: 1, duration: 1, stagger: { repeat: -1, yoyo: true } }
     );
   }, []);
 
   return (
-    <div className="container">
-      <h1 className="first-four">4</h1>
-      <div className="cog-wheel1">
-        <div className="cog1">
-          <div className="top"></div>
-          <div className="down"></div>
-          <div className="left-top"></div>
-          <div className="left-down"></div>
-          <div className="right-top"></div>
-          <div className="right-down"></div>
-          <div className="left"></div>
-          <div className="right"></div>
+    <div className="container-1">
+      <h1 className="first-four-1">4</h1>
+      <div className="cog-wheel1-1">
+        <div className="cog1-1">
+          <div className="top-1"></div>
+          <div className="down-1"></div>
+          <div className="left-top-1"></div>
+          <div className="left-down-1"></div>
+          <div className="right-top-1"></div>
+          <div className="right-down-1"></div>
+          <div className="left-1"></div>
+          <div className="right-1"></div>
         </div>
       </div>
 
-      <div className="cog-wheel2">
-        <div className="cog2">
-          <div className="top"></div>
-          <div className="down"></div>
-          <div className="left-top"></div>
-          <div className="left-down"></div>
-          <div className="right-top"></div>
-          <div className="right-down"></div>
-          <div className="left"></div>
-          <div className="right"></div>
+      <div className="cog-wheel2-1">
+        <div className="cog2-1">
+          <div className="top-1"></div>
+          <div className="down-1"></div>
+          <div className="left-top-1"></div>
+          <div className="left-down-1"></div>
+          <div className="right-top-1"></div>
+          <div className="right-down-1"></div>
+          <div className="left-1"></div>
+          <div className="right-1"></div>
         </div>
       </div>
 
-      <h1 className="second-four">4</h1>
-      <p className="wrong-para">{errorMessage}</p>
+      <h1 className="second-four-1">4</h1>
+      <p className="wrong-para-1">{errorMessage}</p>
     </div>
   );
 };
